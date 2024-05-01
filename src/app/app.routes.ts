979a5d01396be1214f0655692409e97e396a135d
Tangent Chang <andy23512@gmail.com>
2024-05-02 01:09:17 +0800

    feat: add home page

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,6 +1,8 @@
 import { Route } from '@angular/router';
+import { HomePageComponent } from './pages/home-page/home-page.component';
 import { LessonPageComponent } from './pages/lesson-page/lesson-page.component';
 
 export const APP_ROUTES: Route[] = [
   { path: 'topic/:topicId/lesson/:lessonId', component: LessonPageComponent },
+  { path: '', pathMatch: 'full', component: HomePageComponent },
 ];
---
+++ src/app/pages/home-page/home-page.component.ts
@@ -0,0 +1,15 @@
+import { CommonModule } from '@angular/common';
+import { ChangeDetectionStrategy, Component } from '@angular/core';
+import { MatButton } from '@angular/material/button';
+import { MatIcon } from '@angular/material/icon';
+import { RouterLink } from '@angular/router';
+
+@Component({
+  selector: 'app-home-page',
+  standalone: true,
+  imports: [CommonModule, MatButton, MatIcon, RouterLink],
+  templateUrl: './home-page.component.html',
+  styleUrl: './home-page.component.scss',
+  changeDetection: ChangeDetectionStrategy.OnPush,
+})
+export class HomePageComponent {}
