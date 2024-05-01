25dc29212d34abb39255cf67b89eace1344b1c41
Tangent Chang <andy23512@gmail.com>
2024-05-02 01:23:17 +0800

    feat: add disclaimer message and use space to enter first lesson

--- src/app/pages/home-page/home-page.component.ts
+++ src/app/pages/home-page/home-page.component.ts
@@ -1,8 +1,13 @@
 import { CommonModule } from '@angular/common';
-import { ChangeDetectionStrategy, Component } from '@angular/core';
+import {
+  ChangeDetectionStrategy,
+  Component,
+  HostListener,
+  inject,
+} from '@angular/core';
 import { MatButton } from '@angular/material/button';
 import { MatIcon } from '@angular/material/icon';
-import { RouterLink } from '@angular/router';
+import { Router, RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-home-page',
@@ -12,4 +17,12 @@ import { RouterLink } from '@angular/router';
   styleUrl: './home-page.component.scss',
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
-export class HomePageComponent {}
+export class HomePageComponent {
+  readonly router = inject(Router);
+  firstLessonUrl = '/topic/number/lesson/123';
+
+  @HostListener('window:keyup.space')
+  goToFirstLesson() {
+    this.router.navigateByUrl(this.firstLessonUrl);
+  }
+}
