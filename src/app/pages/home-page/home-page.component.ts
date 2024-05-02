09b79ea77281c67e4ea533d7e5df106eb1c2dcc5
Tangent Chang <andy23512@gmail.com>
2024-05-03 01:35:24 +0800

    feat: add background to home page

--- src/app/pages/home-page/home-page.component.ts
+++ src/app/pages/home-page/home-page.component.ts
@@ -9,11 +9,15 @@ import {
 import { MatButton } from '@angular/material/button';
 import { MatIcon } from '@angular/material/icon';
 import { Router, RouterLink } from '@angular/router';
+import { Chance } from 'chance';
+import { SwitchComponent } from 'src/app/components/switch/switch.component';
+
+const chance = new Chance();
 
 @Component({
   selector: 'app-home-page',
   standalone: true,
-  imports: [CommonModule, MatButton, MatIcon, RouterLink],
+  imports: [CommonModule, MatButton, MatIcon, RouterLink, SwitchComponent],
   templateUrl: './home-page.component.html',
   styleUrl: './home-page.component.scss',
   changeDetection: ChangeDetectionStrategy.OnPush,
@@ -21,6 +25,7 @@ import { Router, RouterLink } from '@angular/router';
 export class HomePageComponent {
   readonly router = inject(Router);
   firstLessonUrl = '/topic/number/lesson/123';
+  highlightPositionCodes: number[] = [[-1, -1, 1, 3], [-1, -1, 2, 4], [-1, -1, 6, 8], [-1, -1, 7, 9]].map(list => chance.pickone(list));
 
   @HostBinding('class') classes = 'block relative h-full';
 
