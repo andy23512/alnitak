3be7b6b68e889e7a20652faf3b1e95fae1c366d9
Tangent Chang <andy23512@gmail.com>
2024-05-05 00:54:18 +0800

    feat: fade the combo and speedometer when focus and remove the font size animation of combo

--- src/app/components/combo-counter/combo-counter.component.ts
+++ src/app/components/combo-counter/combo-counter.component.ts
@@ -1,16 +1,11 @@
-import { MediaMatcher } from '@angular/cdk/layout';
 import { CommonModule } from '@angular/common';
 import {
   ChangeDetectionStrategy,
   Component,
   HostBinding,
-  OnDestroy,
-  OnInit,
-  inject,
   input,
-  signal,
 } from '@angular/core';
-import { rank, temperatureToColor } from 'src/app/utils/color.utils';
+import { temperatureToColor } from 'src/app/utils/color.utils';
 
 @Component({
   selector: 'app-combo-counter',
@@ -20,40 +15,12 @@ import { rank, temperatureToColor } from 'src/app/utils/color.utils';
   styleUrl: './combo-counter.component.scss',
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
-export class ComboCounterComponent implements OnInit, OnDestroy {
+export class ComboCounterComponent {
   readonly combo = input.required<number>();
-  readonly noAnimation = signal(false);
-  mediaQuery!: MediaQueryList;
 
-  @HostBinding('class') hostClasses =
-    'text-left transition-[font-size] ease-[cubic-bezier(0.34,1.56,0.64,1)]';
-  @HostBinding('style.font-size') get fontSize() {
-    if (this.noAnimation()) {
-      return '1em';
-    }
-    const combo = this.combo();
-    return 1 + rank(combo) * 0.2 + 'em';
-  }
+  @HostBinding('class') hostClasses = 'text-left';
   @HostBinding('style.color') get color() {
-    const mockC = this.combo();
-    return temperatureToColor(mockC * 100);
+    const c = this.combo();
+    return temperatureToColor(c * 100);
   }
-
-  mediaMatcher = inject(MediaMatcher);
-
-  ngOnInit(): void {
-    this.mediaQuery = this.mediaMatcher.matchMedia(
-      '(prefers-reduced-motion: reduce)',
-    );
-    this.detectReducedMotion();
-    this.mediaQuery.addEventListener('change', this.detectReducedMotion);
-  }
-
-  ngOnDestroy(): void {
-    this.mediaQuery.removeEventListener('change', this.detectReducedMotion);
-  }
-
-  detectReducedMotion = () => {
-    this.noAnimation.set(!!this.mediaQuery.matches);
-  };
 }
