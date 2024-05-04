a205258e198219700558918728444860feef516a
Tangent Chang <andy23512@gmail.com>
2024-05-04 15:03:45 +0800

    feat: let font-size of combo counter not change in reduced motion mode

--- src/app/components/combo-counter/combo-counter.component.ts
+++ src/app/components/combo-counter/combo-counter.component.ts
@@ -1,9 +1,14 @@
+import { MediaMatcher } from '@angular/cdk/layout';
 import { CommonModule } from '@angular/common';
 import {
   ChangeDetectionStrategy,
   Component,
   HostBinding,
+  OnDestroy,
+  OnInit,
+  inject,
   input,
+  signal,
 } from '@angular/core';
 import { rank, temperatureToColor } from 'src/app/utils/color.utils';
 
@@ -15,12 +20,17 @@ import { rank, temperatureToColor } from 'src/app/utils/color.utils';
   styleUrl: './combo-counter.component.scss',
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
-export class ComboCounterComponent {
+export class ComboCounterComponent implements OnInit, OnDestroy {
   readonly combo = input.required<number>();
+  readonly noAnimation = signal(false);
+  mediaQuery!: MediaQueryList;
 
   @HostBinding('class') hostClasses =
     'text-center transition-[font-size] ease-[cubic-bezier(0.34,1.56,0.64,1)]';
   @HostBinding('style.font-size') get fontSize() {
+    if (this.noAnimation()) {
+      return '1em';
+    }
     const combo = this.combo();
     return 1 + rank(combo) * 0.2 + 'em';
   }
@@ -28,4 +38,22 @@ export class ComboCounterComponent {
     const mockC = this.combo();
     return temperatureToColor(mockC * 100);
   }
+
+  mediaMatcher = inject(MediaMatcher);
+
+  ngOnInit(): void {
+    this.mediaQuery = this.mediaMatcher.matchMedia(
+      '(prefers-reduced-motion: reduce)',
+    );
+    this.detectReducedMotion();
+    this.mediaQuery.addEventListener('change', this.detectReducedMotion);
+  }
+
+  ngOnDestroy(): void {
+    this.mediaQuery.removeEventListener('change', this.detectReducedMotion);
+  }
+
+  detectReducedMotion = () => {
+    this.noAnimation.set(!!this.mediaQuery.matches);
+  };
 }
