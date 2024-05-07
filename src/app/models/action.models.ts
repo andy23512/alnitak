955bc036c45f2d5d88f3cd290b9fb5ef1424e497
Tangent Chang <andy23512@gmail.com>
2024-05-08 00:33:58 +0800

    refactor: remove unused actions

--- src/app/models/action.models.ts
+++ src/app/models/action.models.ts
@@ -5,9 +5,5 @@ import { WritingSystemKeyCode } from './writing-system-key-code.models';
  */
 export interface Action {
   codeId: number;
-  type: string;
-  representation: string;
-  description: string;
-  notes: string;
-  writingSystemKeyCode?: WritingSystemKeyCode;
+  writingSystemKeyCode: WritingSystemKeyCode;
 }
