18a769857ba6583731b93d955fcbc0d0aca7faa4
Tangent Chang <andy23512@gmail.com>
2024-05-11 10:45:30 +0800

    fix: fix /?\| symbol lesson not work error

--- src/app/utils/lesson.utils.ts
+++ src/app/utils/lesson.utils.ts
@@ -3,7 +3,7 @@ import { Lesson } from '../models/topic.models';
 export function generateCharacterLesson(str: string): Lesson {
   const components = str.split('');
   return {
-    id: str,
+    id: encodeURIComponent(str),
     name: components.join(' '),
     components,
   };
