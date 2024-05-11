660e5cb87fe352348671a8246c42055925ee480b
Tangent Chang <andy23512@gmail.com>
2024-05-12 00:30:45 +0800

    refactor: reduce calculations in switch sector

--- src/app/components/switch-sector/switch-sector.component.ts
+++ src/app/components/switch-sector/switch-sector.component.ts
@@ -18,8 +18,10 @@ function cos(deg: number) {
   return Math.cos((deg / 180) * Math.PI);
 }
 const o = 8;
-const r1 = 57;
-const r2 = 167;
+const r1 = 65;
+const r2 = 175;
+const alpha1 = (Math.asin(((o / 2) * Math.SQRT2) / r1) / Math.PI) * 180;
+const alpha2 = (Math.asin(((o / 2) * Math.SQRT2) / r2) / Math.PI) * 180;
 
 @Component({
   selector: '[appSwitchSector]',
@@ -42,28 +44,33 @@ export class SwitchSectorComponent {
     const center = this.center();
     const direction = this.direction();
     const d = this.degree();
-    const cx = center.x + o * cos(d);
-    const cy = center.y + o * sin(d);
+    const cx = center.x;
+    const cy = center.y;
+    const dStart = d - 45;
+    const dEnd = d + 45;
+    const beta1Start = dStart + alpha1;
+    const beta1End = dEnd - alpha1;
+    const beta2Start = dStart + alpha2;
+    const beta2End = dEnd - alpha2;
     if (direction === 'cw') {
-      return [
-        `M ${cx + r1 * cos(d - 45)} ${cy + r1 * sin(d - 45)}`,
-        `A ${o + r1} ${o + r1} 0 0 1 ${cx + r1 * cos(d + 45)} ${cy + r1 * sin(d + 45)}`,
-        `L ${cx + r2 * cos(d + 45)} ${cy + r2 * sin(d + 45)}`,
-        `A ${o + r2} ${o + r2} 0 0 0 ${cx + r2 * cos(d - 45)} ${cy + r2 * sin(d - 45)}`,
-      ].join(' ');
+      return `
+        M ${cx + r1 * cos(beta1Start)} ${cy + r1 * sin(beta1Start)}
+        A ${r1} ${r1} 0 0 1 ${cx + r1 * cos(beta1End)} ${cy + r1 * sin(beta1End)}
+        L ${cx + r2 * cos(beta2End)} ${cy + r2 * sin(beta2End)}
+        A ${r2} ${r2} 0 0 0 ${cx + r2 * cos(beta2Start)} ${cy + r2 * sin(beta2Start)}
+      `;
     } else {
-      return [
-        `M ${cx + r1 * cos(d + 45)} ${cy + r1 * sin(d + 45)}`,
-        `A ${o + r1} ${o + r1} 0 0 0 ${cx + r1 * cos(d - 45)} ${cy + r1 * sin(d - 45)}`,
-        `L ${cx + r2 * cos(d - 45)} ${cy + r2 * sin(d - 45)}`,
-        `A ${o + r2} ${o + r2} 0 0 1 ${cx + r2 * cos(d + 45)} ${cy + r2 * sin(d + 45)}`,
-      ].join(' ');
+      return `
+        M ${cx + r1 * cos(beta1End)} ${cy + r1 * sin(beta1End)}
+        A ${r1} ${r1} 0 0 0 ${cx + r1 * cos(beta1Start)} ${cy + r1 * sin(beta1Start)}
+        L ${cx + r2 * cos(beta2Start)} ${cy + r2 * sin(beta2Start)}
+        A ${r2} ${r2} 0 0 1 ${cx + r2 * cos(beta2End)} ${cy + r2 * sin(beta2End)}
+      `;
     }
   });
 
   textRadius = (() => {
-    const ratio = 0.55;
-    return (1 - ratio) * r1 + ratio * r2;
+    return (r1 + r2) / 2;
   })();
 
   readonly textX = computed(() => {
