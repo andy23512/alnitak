25a617f874e1773cc28407cf0f99f9154856a9a2
Tangent Chang <andy23512@gmail.com>
2024-05-21 01:19:08 +0800

    feat: add empty highstock chart

--- src/app/pages/statistics-page/statistics-page.component.ts
+++ src/app/pages/statistics-page/statistics-page.component.ts
@@ -1,11 +1,20 @@
-import { CommonModule } from '@angular/common';
 import { ChangeDetectionStrategy, Component } from '@angular/core';
+import { HighchartsChartModule } from 'highcharts-angular';
+import * as Highcharts from 'highcharts/highstock';
+import theme from 'highcharts/themes/high-contrast-dark';
+theme(Highcharts);
 
 @Component({
   selector: 'app-statistics-page',
   standalone: true,
-  imports: [CommonModule],
+  imports: [HighchartsChartModule],
   templateUrl: './statistics-page.component.html',
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
-export class StatisticsPageComponent {}
+export class StatisticsPageComponent {
+  Highcharts: typeof Highcharts = Highcharts; // required
+  chartConstructor: string = 'stockChart'; // optional string, defaults to 'chart'
+  chartOptions: Highcharts.Options = {
+    series: [{ type: 'line', data: [1, 2] }],
+  }; // required
+}
