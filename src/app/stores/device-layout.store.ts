b90cee9985f21a8cc761b629420a118655b50a8e
Tangent Chang <andy23512@gmail.com>
2024-10-12 09:04:36 +0800

    feat: add M4G default layout

--- src/app/stores/device-layout.store.ts
+++ src/app/stores/device-layout.store.ts
@@ -3,8 +3,16 @@ import {
   withStorageSync,
 } from '@angular-architects/ngrx-toolkit';
 import { patchState, signalStore, withHooks, withMethods } from '@ngrx/signals';
-import { setAllEntities, withEntities } from '@ngrx/signals/entities';
-import { DEFAULT_DEVICE_LAYOUT } from '../data/device-layouts';
+import {
+  addEntity,
+  setAllEntities,
+  updateEntity,
+  withEntities,
+} from '@ngrx/signals/entities';
+import {
+  DEFAULT_DEVICE_LAYOUT,
+  M4G_DEFAULT_DEVICE_LAYOUT,
+} from '../data/device-layouts';
 import { DeviceLayout } from '../models/device-layout.models';
 import { withSelectedEntity } from './selected-entity.feature';
 
@@ -16,14 +24,29 @@ export const DeviceLayoutStore = signalStore(
   withSelectedEntity(),
   withMethods((store) => ({
     load() {
-      patchState(store, setAllEntities([DEFAULT_DEVICE_LAYOUT]));
+      patchState(
+        store,
+        setAllEntities([DEFAULT_DEVICE_LAYOUT, M4G_DEFAULT_DEVICE_LAYOUT]),
+      );
       store.setSelectedId(DEFAULT_DEVICE_LAYOUT.id);
     },
   })),
   withHooks({
     onInit: (store) => {
       if (store.selectedId() === null) {
-        store.load();
+        return store.load();
+      }
+      const entities = store.entities();
+      const defaultLayout = entities.find((e) => e.id === 'default');
+      if (defaultLayout && defaultLayout.name === 'Default') {
+        patchState(
+          store,
+          updateEntity({ id: 'default', changes: { name: 'CC1 Default' } }),
+        );
+      }
+      const m4gDefaultLayout = entities.find((e) => e.id === 'm4g-default');
+      if (!m4gDefaultLayout) {
+        patchState(store, addEntity(M4G_DEFAULT_DEVICE_LAYOUT));
       }
     },
   }),
