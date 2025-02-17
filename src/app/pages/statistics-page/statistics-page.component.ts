f2257be1abae73aa70577dc4f6f91cfbab72abf6
Tangent Chang <andy23512@gmail.com>
2025-02-18 00:16:42 +0800

    feat: add combo metric to statistics

--- src/app/pages/statistics-page/statistics-page.component.ts
+++ src/app/pages/statistics-page/statistics-page.component.ts
@@ -1,4 +1,12 @@
-import { ChangeDetectionStrategy, Component, computed } from '@angular/core';
+import {
+  ChangeDetectionStrategy,
+  Component,
+  computed,
+  HostBinding,
+  signal,
+} from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { MatButtonToggleModule } from '@angular/material/button-toggle';
 import { liveQuery } from 'dexie';
 import { HighchartsChartModule } from 'highcharts-angular';
 import * as Highcharts from 'highcharts/highstock';
@@ -10,15 +18,24 @@ import { KeyRecord } from 'src/app/models/key-record.models';
 import { computedAsync } from 'src/app/utils/computed-async.utils';
 theme(Highcharts);
 
+enum Metric {
+  CPM = 'CPM',
+  Combo = 'Combo',
+}
+
 @Component({
   selector: 'app-statistics-page',
   standalone: true,
-  imports: [HighchartsChartModule],
+  imports: [HighchartsChartModule, MatButtonToggleModule, FormsModule],
   templateUrl: './statistics-page.component.html',
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
 export class StatisticsPageComponent {
+  @HostBinding('class') classes = 'flex flex-col gap-2 h-screen';
+
   Highcharts: typeof Highcharts = Highcharts;
+  Metric = Metric;
+  currentMetric = signal(Metric.CPM);
   chartConstructor: string = 'stockChart';
   updateFlag = false;
 
@@ -32,42 +49,98 @@ export class StatisticsPageComponent {
     if (!keyRecords) {
       return null;
     }
-    const series: Highcharts.SeriesOptionsType[] = TOPICS.map((t) => ({
-      type: 'line' as const,
-      data: keyRecords
-        .filter((k) => k.cpm && k.topicId === t.id)
-        .map((k) => [k.timestamp, k.cpm]),
-      name: t.name,
-      dataGrouping: {
-        groupPixelWidth: 20,
-      },
-      marker: {
-        enabled: true,
-        radius: 5,
-      },
-    })).filter((s) => s.data.length > 0);
-    return {
-      legend: { enabled: true },
-      scrollbar: { enabled: false },
-      series,
-      time: {
-        useUTC: false
-      },
-      tooltip: {
-        formatter: function () {
-          return `${this.series.name}: ${this.y?.toFixed(1)}`
-        }
-      },
-      yAxis: {
-        title: {
-          text: 'CPM',
-        },
-      },
-      xAxis: {
-        type: 'datetime',
-        ordinal: false,
-        breaks: undefined,
-      },
-    };
+    const currentMetric = this.currentMetric();
+    switch (currentMetric) {
+      case Metric.CPM:
+        const cpmSeries: Highcharts.SeriesOptionsType[] = TOPICS.map((t) => ({
+          type: 'line' as const,
+          data: keyRecords
+            .filter((k) => k.cpm && k.topicId === t.id)
+            .map((k) => [k.timestamp, k.cpm]),
+          name: t.name,
+          dataGrouping: {
+            groupPixelWidth: 20,
+          },
+          marker: {
+            enabled: true,
+            radius: 5,
+          },
+        })).filter((s) => s.data.length > 0);
+        return {
+          legend: { enabled: true },
+          scrollbar: { enabled: false },
+          series: cpmSeries,
+          time: {
+            useUTC: false,
+          },
+          tooltip: {
+            formatter: function () {
+              return `${this.series.name}: ${this.y?.toFixed(1)} CPM`;
+            },
+          },
+          yAxis: {
+            title: {
+              text: 'CPM',
+            },
+          },
+          xAxis: {
+            type: 'datetime',
+            ordinal: false,
+            breaks: undefined,
+          },
+        };
+      case Metric.Combo:
+        const comboSeries: Highcharts.SeriesOptionsType[] = TOPICS.map((t) => ({
+          type: 'line' as const,
+          data: keyRecords
+            .filter((k) => k.combo && k.topicId === t.id)
+            .map((k) => [k.timestamp, k.combo]),
+          name: t.name,
+          dataGrouping: {
+            groupPixelWidth: 20,
+            approximation: function (this: {
+              dataGroupInfo: { start: number; length: number };
+              options: { data: [number, number][] };
+            }): number {
+              const start = this.dataGroupInfo.start;
+              return Math.max(
+                ...this.options.data
+                  .slice(start, start + this.dataGroupInfo.length)
+                  .map(([_, combo]) => combo),
+              );
+            },
+          },
+          marker: {
+            enabled: true,
+            radius: 5,
+          },
+        })).filter((s) => s.data.length > 0);
+        return {
+          legend: { enabled: true },
+          scrollbar: { enabled: false },
+          series: comboSeries,
+          time: {
+            useUTC: false,
+          },
+          tooltip: {
+            formatter: function () {
+              return `${this.series.name}: ${Math.round(this.y as number)} Combos`;
+            },
+          },
+          yAxis: {
+            title: {
+              text: 'Combo',
+            },
+          },
+          xAxis: {
+            type: 'datetime',
+            ordinal: false,
+            breaks: undefined,
+          },
+        };
+      default:
+        const _: never = currentMetric;
+        throw new Error(`Unhandled metric case: ${currentMetric}`);
+    }
   });
 }
