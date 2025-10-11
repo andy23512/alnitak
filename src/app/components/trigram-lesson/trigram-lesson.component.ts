b346490a7bde4153e23ebab6408225797377f0c2
Tangent Chang <andy23512@gmail.com>
2025-10-11 19:55:18 +0800

    feat: implement basic ui for trigram lesson

--- src/app/components/trigram-lesson/trigram-lesson.component.ts
+++ src/app/components/trigram-lesson/trigram-lesson.component.ts
@@ -1,12 +1,52 @@
-import { ChangeDetectionStrategy, Component, input } from '@angular/core';
+import {
+  ChangeDetectionStrategy,
+  Component,
+  ElementRef,
+  input,
+  signal,
+  viewChild,
+} from '@angular/core';
+import { MatButton, MatIconButton } from '@angular/material/button';
+import { MatIcon } from '@angular/material/icon';
+import { MatTooltipModule } from '@angular/material/tooltip';
+import { RouterLinkWithHref } from '@angular/router';
+import { LetDirective } from '@ngrx/component';
 import { TrigramLessonWithPreviousAndNextLessonUrl } from 'src/app/models/topic.models';
+import { IconGuardPipe } from 'src/app/pipes/icon-guard.pipe';
 
 @Component({
   selector: 'accel-shooter-trigram-lesson',
   standalone: true,
   templateUrl: 'trigram-lesson.component.html',
   changeDetection: ChangeDetectionStrategy.OnPush,
+  imports: [
+    IconGuardPipe,
+    LetDirective,
+    MatButton,
+    MatIcon,
+    MatIconButton,
+    MatTooltipModule,
+    RouterLinkWithHref,
+  ],
 })
 export class TrigramLessonComponent {
   lesson = input.required<TrigramLessonWithPreviousAndNextLessonUrl>();
+
+  readonly isFocus = signal(false);
+
+  public input = viewChild.required<ElementRef<HTMLInputElement>>('input');
+
+  onInput(event: InputEvent) {
+    console.log(event);
+    // TODO - handle input
+  }
+
+  startLesson() {
+    this.input().nativeElement.focus();
+    // TODO - add air type support
+  }
+
+  pauseLesson() {
+    // TODO - handle pause lesson
+  }
 }
