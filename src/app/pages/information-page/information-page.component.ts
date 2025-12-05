8821d0b16368d19bcd69ecb22d2ec63a5daa852e
Tangent Chang <andy23512@gmail.com>
2025-12-05 21:51:10 +0800

    feat: add layout song page to information

--- src/app/pages/information-page/information-page.component.ts
+++ src/app/pages/information-page/information-page.component.ts
@@ -1,11 +1,12 @@
 import { ChangeDetectionStrategy, Component, HostBinding } from '@angular/core';
 import { MatIcon } from '@angular/material/icon';
 import { RouterLink } from '@angular/router';
+import { IconGuardPipe } from 'src/app/pipes/icon-guard.pipe';
 
 @Component({
   selector: 'app-information-page',
   standalone: true,
-  imports: [RouterLink, MatIcon],
+  imports: [RouterLink, MatIcon, IconGuardPipe],
   templateUrl: './information-page.component.html',
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
