c6d51fe7e177afc498f44595c0db66994cabd6ce
Tangent Chang <andy23512@gmail.com>
2025-12-06 20:50:48 +0800

    feat: add translation to combo counter component

--- src/app/components/combo-counter/combo-counter.component.ts
+++ src/app/components/combo-counter/combo-counter.component.ts
@@ -1,9 +1,11 @@
+import { UpperCasePipe } from '@angular/common';
 import {
   ChangeDetectionStrategy,
   Component,
   HostBinding,
   input,
 } from '@angular/core';
+import { TranslatePipe } from '@ngx-translate/core';
 import { PARAMETER } from 'src/app/data/parameters';
 import { temperatureToColor } from 'src/app/utils/color.utils';
 import { SevenSegmentComponent } from '../seven-segment/seven-segment.component';
@@ -11,7 +13,7 @@ import { SevenSegmentComponent } from '../seven-segment/seven-segment.component'
 @Component({
   selector: 'app-combo-counter',
   standalone: true,
-  imports: [SevenSegmentComponent],
+  imports: [SevenSegmentComponent, UpperCasePipe, TranslatePipe],
   templateUrl: './combo-counter.component.html',
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
