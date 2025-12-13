abc0c33c6943d754ddbb780b4933f8cc493691e3
Tangent Chang <andy23512@gmail.com>
2025-12-13 10:49:57 +0800

    feat: add translation to visibility setting panel content component

--- src/app/components/visibility-setting-panel-content/visibility-setting-panel-content.component.ts
+++ src/app/components/visibility-setting-panel-content/visibility-setting-panel-content.component.ts
@@ -8,8 +8,10 @@ import { MatIconButton } from '@angular/material/button';
 import { MatCheckbox } from '@angular/material/checkbox';
 import { MatDialog } from '@angular/material/dialog';
 import { MatIcon } from '@angular/material/icon';
+import { TranslatePipe } from '@ngx-translate/core';
 import { VisibilitySetting } from 'src/app/models/visibility-setting.models';
 import { IconGuardPipe } from 'src/app/pipes/icon-guard.pipe';
+import { RealTitleCasePipe } from 'src/app/pipes/real-title-case.pipe';
 import { VisibilitySettingStore } from 'src/app/stores/visibility-setting.store';
 import { KeyNotationHelpDialogComponent } from '../key-notation-help-dialog/key-notation-help-dialog.component';
 import { Thumb3SwitchHelpDialogComponent } from '../thumb-3-switch-help-dialog/thumb-3-switch-help-dialog.component';
@@ -18,19 +20,35 @@ const VISIBILITY_SETTING_ITEMS: {
   name: string;
   key: keyof VisibilitySetting;
 }[] = [
-  { name: 'Layout', key: 'layout' },
-  { name: 'Layout - Text Guide', key: 'layoutTextGuide' },
-  { name: 'Layout - Key Notation Guide', key: 'layoutKeyNotationGuide' },
-  { name: 'Layout - Thumb 3 Switch', key: 'layoutThumb3Switch' },
-  { name: 'Combo Counter', key: 'comboCounter' },
-  { name: 'Speedometer', key: 'speedometer' },
-  { name: 'Home Page Chording Animation', key: 'homePageChordingAnimation' },
+  { name: 'visibility-setting.layout', key: 'layout' },
+  { name: 'visibility-setting.layout-text-guide', key: 'layoutTextGuide' },
+  {
+    name: 'visibility-setting.layout-key-notation-guide',
+    key: 'layoutKeyNotationGuide',
+  },
+  {
+    name: 'visibility-setting.layout-thumb-3-switch',
+    key: 'layoutThumb3Switch',
+  },
+  { name: 'visibility-setting.combo-counter', key: 'comboCounter' },
+  { name: 'visibility-setting.speedometer', key: 'speedometer' },
+  {
+    name: 'visibility-setting.home-page-chording-animation',
+    key: 'homePageChordingAnimation',
+  },
 ];
 
 @Component({
   selector: 'app-visibility-setting-panel-content',
   standalone: true,
-  imports: [MatCheckbox, MatIconButton, MatIcon, IconGuardPipe],
+  imports: [
+    MatCheckbox,
+    MatIconButton,
+    MatIcon,
+    IconGuardPipe,
+    TranslatePipe,
+    RealTitleCasePipe,
+  ],
   templateUrl: './visibility-setting-panel-content.component.html',
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
