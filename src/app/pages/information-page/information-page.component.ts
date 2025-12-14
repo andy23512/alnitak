f92fb3a1f43670a1fb55079c69423ba09c2671c1
Tangent Chang <andy23512@gmail.com>
2025-12-14 11:22:08 +0800

    feat: add translation for information page

--- src/app/pages/information-page/information-page.component.ts
+++ src/app/pages/information-page/information-page.component.ts
@@ -1,12 +1,20 @@
 import { ChangeDetectionStrategy, Component, HostBinding } from '@angular/core';
 import { MatIcon } from '@angular/material/icon';
 import { RouterLink } from '@angular/router';
+import { TranslatePipe } from '@ngx-translate/core';
 import { IconGuardPipe } from 'src/app/pipes/icon-guard.pipe';
+import { RealTitleCasePipe } from 'src/app/pipes/real-title-case.pipe';
 
 @Component({
   selector: 'app-information-page',
   standalone: true,
-  imports: [RouterLink, MatIcon, IconGuardPipe],
+  imports: [
+    RouterLink,
+    MatIcon,
+    IconGuardPipe,
+    TranslatePipe,
+    RealTitleCasePipe,
+  ],
   templateUrl: './information-page.component.html',
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
